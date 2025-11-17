e6fda22660feefd92963f6d16b28247ee9ca7898
Olena457 <olenademianenko42@gmail.com>
2025-11-17 16:35:33 +0200

    Create storage.js

---
+++ src/utils/storage.js
@@ -0,0 +1,27 @@
+import { defaultAppState, hydrateState } from "../data/defaultAppState.js";
+const KEY_STORAGE = 'viteTodoAppState';
+export function saveState(state) {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem(KEY_STORAGE, serializedState);
+    } catch (err) {
+        console.error("Error saving state", err);
+        
+    }
+}
+
+export function loadState() {
+    const serializedState = localStarage.getItem(KEY_STORAGE);
+    if (serializedState === null) {
+        console.log("local Storage empty.Used initial state");
+        return defaultAppState;
+    } try {
+
+        const rawData = JSON.parse(serializedState);
+        return hydrateState(rawData);
+
+    } catch (err) {
+        console.error("Error loading data:", err);
+        return defaultAppState;
+    }
+}
